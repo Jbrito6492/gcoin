6884f84f376096c7518837769f4e68d26231d94c
Josh Brito <jbrito6492@gmail.com>
2021-05-05 22:05:29 -0700

    added is_valid_block method

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -78,11 +78,42 @@ class Block:
 
         return 1
 
+    @staticmethod
+    def is_valid_block(last_block, block):
+        """
+        Validate a block by enforcing following rules:
+          - block must have proper last_hash reference
+          - block must meet proof of work requirement
+          - difficulty must only adjust by 1
+          - block hash must be valid combination of block fields
+        """
+        if block.last_hash != last_block.hash:
+            raise Exception('block last_hash must be correct')
+
+        if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
+            raise Exception('proof of work requirement was not met')
+        
+        if abs(last_block.difficulty - block.difficulty) > 1:
+            raise Exception('difficulty was tampered with')
+
+        reconstructed_hash = crypto_hash(
+            block.timestamp,
+            block.last_hash,
+            block.data,
+            block.nonce,
+            block.difficulty
+        )
+        if block.hash != reconstructed_hash:
+            raise Exception('block hash must be valid')
 
 def main():
     genesis_block = Block.genesis()
-    block = Block.mine_block(genesis_block, 'foo')
-    print(block)
+    bad_block = Block.mine_block(genesis_block, 'foo')
+    bad_block.last_hash = 'corrupt_data'
+    try:
+        Block.is_valid_block(genesis_block, bad_block)
+    except Exception as e:
+        print(f"is_valid_block: {e}")
 
 
 if __name__ == '__main__':
